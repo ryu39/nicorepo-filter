63d888339f5725e9e98e943589b92921c6ad699e
ryu39 <ryu.honmax39@gmail.com>
2016-06-04 23:35:21 +0900

    Bump version

--- nicorepo-filter.user.js
+++ nicorepo-filter.user.js
@@ -10,7 +10,7 @@
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_registerMenuCommand
-// @version     1.0.5
+// @version     1.0.6
 // @downloadURL https://github.com/ryu39/nicorepo-filter/raw/master/nicorepo-filter.user.js
 // @updateURL   https://github.com/ryu39/nicorepo-filter/raw/master/nicorepo-filter.user.js
 // ==/UserScript==
